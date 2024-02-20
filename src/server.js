ed4042641748871420a832261b4af64fe4f36b1b
Nicolas Santiago Del Vecchio <nnicou.s@gmail.com>
2024-02-20 20:08:50 -0300

    register

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const authRoutes = require("./routes/auth.routes");
-const taskRoutes = require("./routes/tasks.routes")
-
+const taskRoutes = require("./routes/tasks.routes");
+const cors = require("cors");
 const server = express();
 
+server.use(cors({
+    origin: "http://localhost:5173"
+}))
 server.use(express.json());
 server.use(morgan("dev"));
 server.use(cookieParser());
